0f4cf057c73f2c4e79b2b7fbe27bc7b59552e056
= <janusonis.gustas@gmail.com>
2025-10-15 22:31:52 +0300

    api url test

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,20 +1,20 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
 
-	kit: {
-		// Use static adapter to generate static files for Flask to serve
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html'
-		})
-	}
+  kit: {
+    // Use static adapter to generate static files for Flask to serve
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
+    }),
+  },
 };
 
 export default config;
